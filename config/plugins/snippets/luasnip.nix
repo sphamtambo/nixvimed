5d45576f03afcb0927af6c90c2b4d9876ac5da0a
sphamtambo <sphamtambo@gmail.com>
2025-07-11 12:46:09 +0200

    fixed luasnip settings

--- config/plugins/snippets/luasnip.nix
+++ config/plugins/snippets/luasnip.nix
@@ -1,8 +1,10 @@
 {pkgs, ...}: {
   plugins.luasnip = {
     enable = true;
-    enable_autosnippets = true;
-    store_selection_keys = "<Tab>";
+    settings = {
+      enable_autosnippets = true;
+      store_selection_keys = "<Tab>";
+    };
     fromVscode = [
       {
         lazyLoad = true;
